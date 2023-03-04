36bb564b5a1fc639083906f2657b31e3142fe956
wioenena-q <wioenena@gmail.com>
2023-03-04 16:05:11 +0300

    feat: basic methods written

--- deno/lib/Decoder.ts
+++ deno/lib/Decoder.ts
@@ -1,1 +1,81 @@
-export class Decoder {}
+export class Decoder {
+  private buffer = new Uint8Array(0);
+  private view = new DataView(this.buffer.buffer);
+  private offset = 0;
+
+  /**
+   * Decodes data.
+   * @param data Data to decode.
+   */
+  public decode(data: Uint8Array) {
+    this.buffer = data;
+    this.view = new DataView(this.buffer.buffer);
+    this.offset = 0;
+    const decoded = this.nativeDecode(data);
+    this.reset();
+    return decoded;
+  }
+
+  /**
+   * For recursive calls to decode.
+   * @param data Data to decode.
+   */
+  private nativeDecode(data: Uint8Array) {
+  }
+
+  private reset() {
+    this.buffer = new Uint8Array(0);
+    this.view = new DataView(this.buffer.buffer);
+    this.offset = 0;
+  }
+
+  private readInt8() {
+    return this.view.getInt8(this.offset++);
+  }
+
+  private readUInt8() {
+    return this.view.getUint8(this.offset++);
+  }
+
+  private readInt16(littleEndian = false) {
+    const value = this.view.getInt16(this.offset, littleEndian);
+    this.offset += 2;
+    return value;
+  }
+
+  private readUInt16(littleEndian = false) {
+    const value = this.view.getUint16(this.offset, littleEndian);
+    this.offset += 2;
+    return value;
+  }
+
+  private readInt32(littleEndian = false) {
+    const value = this.view.getInt32(this.offset, littleEndian);
+    this.offset += 4;
+    return value;
+  }
+
+  private readUInt32(littleEndian = false) {
+    const value = this.view.getUint32(this.offset, littleEndian);
+    this.offset += 4;
+    return value;
+  }
+
+  private readFloat64(littleEndian = false) {
+    const value = this.view.getFloat64(this.offset, littleEndian);
+    this.offset += 8;
+    return value;
+  }
+
+  private readBigInt64(littleEndian = false) {
+    const value = this.view.getBigInt64(this.offset, littleEndian);
+    this.offset += 8;
+    return value;
+  }
+
+  private readBigUint64(littleEndian = false) {
+    const value = this.view.getBigUint64(this.offset, littleEndian);
+    this.offset += 8;
+    return value;
+  }
+}
