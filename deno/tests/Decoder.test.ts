fb6d65e14e93b595ee23b62b05cdb5bedcf5c2a3
wioenena-q <wioenena@gmail.com>
2023-03-04 16:28:13 +0300

    test: SMALL_INTEGER_EXT term decoding is tested

--- deno/tests/Decoder.test.ts
+++ deno/tests/Decoder.test.ts
@@ -1,1 +1,28 @@
-Deno.test("Decoder", () => {});
+import { decode } from "../mod.ts";
+import {
+  dirname,
+  fromFileUrl,
+  join,
+} from "https://deno.land/std@0.178.0/path/mod.ts";
+import {
+  assert,
+  assertThrows,
+} from "https://deno.land/std@0.178.0/testing/asserts.ts";
+
+const __dirname = dirname(fromFileUrl(import.meta.url));
+const encoded_data_dir = join(__dirname, "..", "..", "encoded_data");
+
+Deno.test("Decoder", async (t) => {
+  await t.step("decode with invalid ETF version", () => {
+    assertThrows(() => decode(new Uint8Array([0])), Error);
+  });
+
+  await t.step("decode SMALL_INTEGER_EXT", () => {
+    const encoded = Deno.readFileSync(
+      join(encoded_data_dir, "SMALL_INTEGER_EXT"),
+    );
+
+    const decoded = decode(encoded);
+    assert(decoded === 255);
+  });
+});
