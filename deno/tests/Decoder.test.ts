5ad2438b779f9714a3ad17c3ba973c9ba88548db
wioenena-q <wioenena@gmail.com>
2023-03-05 15:30:06 +0300

    test: Decode tests of all terms have been done

--- deno/tests/Decoder.test.ts
+++ deno/tests/Decoder.test.ts
@@ -8,7 +8,14 @@ import {
   assertEquals,
   assertThrows,
 } from "https://deno.land/std@0.178.0/testing/asserts.ts";
-import { IPid, IPort, IReference } from "../lib/Structs/types.d.ts";
+import { Atom } from "../lib/Structs/Atom.ts";
+import {
+  IExport,
+  INewFun,
+  IPid,
+  IPort,
+  IReference,
+} from "../lib/Structs/types.d.ts";
 
 import { decode } from "../mod.ts";
 
@@ -27,18 +34,21 @@ Deno.test("Decoder", async (t) => {
   await t.step("decode SMALL_INTEGER_EXT", () => {
     const encoded = getEncodedData("SMALL_INTEGER_EXT");
     const decoded = decode(encoded);
+
     assertEquals(decoded, 255);
   });
 
   await t.step("decode INTEGER_EXT", () => {
     const encoded = getEncodedData("INTEGER_EXT");
     const decoded = decode(encoded);
+
     assertEquals(decoded, 2 ** 10);
   });
 
   await t.step("decode FLOAT_EXT", () => {
     const encoded = getEncodedData("FLOAT_EXT");
     const decoded = decode(encoded);
+
     assertEquals(decoded, 3.14);
   });
 
@@ -67,6 +77,7 @@ Deno.test("Decoder", async (t) => {
   await t.step("decode PID_EXT", () => {
     const encoded = getEncodedData("PID_EXT");
     const decoded = decode(encoded) as IPid;
+
     assertEquals(decoded.toString(), "#PID<0.106.0>");
   });
 
@@ -77,15 +88,45 @@ Deno.test("Decoder", async (t) => {
   });
 
   await t.step("decode SMALL_TUPLE_EXT", () => {
-    console.log("TODO: Test SMALL_TUPLE_EXT");
+    const encoded = getEncodedData("SMALL_TUPLE_EXT");
+    const decoded = decode(encoded) as unknown[];
+
+    assertEquals(decoded, [
+      "Hello",
+      "World",
+      2 ** 10,
+      ["Hello", "World"],
+      {
+        Hello: "World",
+      },
+    ]);
   });
 
   await t.step("decode LARGE_TUPLE_EXT", () => {
-    console.log("TODO: Test LARGE_TUPLE_EXT");
+    const encoded = getEncodedData("LARGE_TUPLE_EXT");
+    const decoded = decode(encoded) as unknown[];
+
+    assertEquals(decoded, [
+      "Hello",
+      "World",
+      2 ** 10,
+      ["Hello", "World"],
+      {
+        Hello: "World",
+      },
+    ]);
   });
 
   await t.step("decode MAP_EXT", () => {
-    console.log("TODO: Test MAP_EXT");
+    const encoded = getEncodedData("MAP_EXT");
+    const decoded = decode(encoded) as Record<string, unknown>;
+    assertEquals(decoded, {
+      atom: new Atom("atom", false),
+      list: ["Hello", "World", 2 ** 10],
+      number: 2 ** 10,
+      string: "Hello World",
+      tuple: ["Hello", "World", 2 ** 10],
+    });
   });
 
   await t.step("decode NIL_EXT", () => {
@@ -102,7 +143,9 @@ Deno.test("Decoder", async (t) => {
   });
 
   await t.step("decode LIST_EXT", () => {
-    console.log("TODO: Test LIST_EXT");
+    const encoded = getEncodedData("LIST_EXT");
+    const decoded = decode(encoded) as unknown as string[];
+    assertEquals(decoded, ["Hello", "World"]);
   });
 
   await t.step("decode BINARY_EXT", () => {
@@ -146,34 +189,67 @@ Deno.test("Decoder", async (t) => {
   });
 
   await t.step("decode NEW_FUN_EXT", () => {
-    console.log("TODO: Test NEW_FUN_EXT");
+    const encoded = getEncodedData("NEW_FUN_EXT");
+    const decoded = decode(encoded) as INewFun;
+    const pid = decoded.pid as IPid;
+    assertEquals(decoded.size, 3619);
+    assertEquals(decoded.arity, 5);
+    assertEquals(decoded.uniq, "065361aa461590d1bdf1eb62d1a6eb08");
+    assertEquals(decoded.index, 38);
+    assertEquals(decoded.numFree, 1);
+    assertEquals(decoded.module, new Atom("erl_eval", false));
+    assertEquals(decoded.oldIndex, 38);
+    assertEquals(decoded.oldUniq, 3316493);
+    assertEquals(pid.node, new Atom("nonode@nohost", false));
+    assertEquals(pid.id, 106);
+    assertEquals(pid.serial, 0);
+    assertEquals(pid.creation, 0);
+    assert(Array.isArray(decoded.freeVars));
   });
 
   await t.step("decode EXPORT_EXT", () => {
-    console.log("TODO: Test EXPORT_EXT");
+    const encoded = getEncodedData("EXPORT_EXT");
+    const decoded = decode(encoded) as IExport;
+    assertEquals(decoded.toString(), "&:Elixir.File.read/1");
   });
 
   await t.step("decode BIT_BINARY_EXT", () => {
-    console.log("TODO: Test BIT_BINARY_EXT");
+    const encoded = getEncodedData("BIT_BINARY_EXT");
+    const decoded = decode(encoded) as number[];
+    assertEquals(decoded, [1, 0, 1, 0, 1, 0, 1, 0]);
   });
 
   await t.step("decode NEW_FLOAT_EXT", () => {
-    console.log("TODO: Test NEW_FLOAT_EXT");
+    const encoded = getEncodedData("NEW_FLOAT_EXT");
+    const decoded = decode(encoded);
+    assertEquals(decoded, Math.PI);
   });
 
   await t.step("decode ATOM_UTF8_EXT", () => {
-    console.log("TODO: Test ATOM_UTF8_EXT");
+    const encoded = getEncodedData("ATOM_UTF8_EXT");
+    const decoded = decode(encoded) as Atom;
+    assertEquals(decoded.toString(), ":atom");
+    assert(decoded.isUTF8);
   });
 
   await t.step("decode SMALL_ATOM_UTF8_EXT", () => {
-    console.log("TODO: Test SMALL_ATOM_UTF8_EXT");
+    const encoded = getEncodedData("SMALL_ATOM_UTF8_EXT");
+    const decoded = decode(encoded) as Atom;
+    assertEquals(decoded.toString(), ":atom");
+    assert(decoded.isUTF8);
   });
 
   await t.step("decode ATOM_EXT", () => {
-    console.log("TODO: Test ATOM_EXT");
+    const encoded = getEncodedData("ATOM_EXT");
+    const decoded = decode(encoded) as Atom;
+    assertEquals(decoded.toString(), ":atom");
+    assert(!decoded.isUTF8);
   });
 
   await t.step("decode SMALL_ATOM_EXT", () => {
-    console.log("TODO: Test SMALL_ATOM_EXT");
+    const encoded = getEncodedData("SMALL_ATOM_EXT");
+    const decoded = decode(encoded) as Atom;
+    assertEquals(decoded.toString(), ":atom");
+    assert(!decoded.isUTF8);
   });
 });
