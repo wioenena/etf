3b2985a1d3cba77bf23ce106cec8ebc553fca4b7
wioenena-q <wioenena@gmail.com>
2023-03-05 07:18:04 +0300

    refactor: renamed enum members

--- deno/lib/Structs/Atom.ts
+++ deno/lib/Structs/Atom.ts
@@ -1,13 +1,13 @@
 export class Atom {
   public constructor(
     public readonly value: string,
-    public readonly term: AtomTerm = AtomTerm.ATOM,
+    public readonly term: AtomTerm = AtomTerm.ATOM_EXT,
   ) {
     if (value.length > 255) {
       throw new Error("Atom cannot be longer than 255 characters");
     }
 
-    if (term === AtomTerm.ATOM || term === AtomTerm.SMALL_ATOM) {
+    if (term === AtomTerm.ATOM_EXT || term === AtomTerm.SMALL_ATOM_EXT) {
       for (const char of value) {
         const charCode = char.charCodeAt(0);
         if (charCode > 0x7F || charCode < 0) {
@@ -19,8 +19,8 @@ export class Atom {
 }
 
 export const enum AtomTerm {
-  ATOM,
-  SMALL_ATOM,
-  ATOM_UTF8,
-  SMALL_ATOM_UTF8,
+  ATOM_EXT,
+  SMALL_ATOM_EXT,
+  ATOM_UTF8_EXT,
+  SMALL_ATOM_UTF8_EXT,
 }
