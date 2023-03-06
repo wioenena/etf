3acd48f0a2f866aba57ac78b437697f2d023ecb4
wioenena-q <wioenena@gmail.com>
2023-03-07 00:55:33 +0300

    feat: encode function exported

--- deno/mod.ts
+++ deno/mod.ts
@@ -1,3 +1,3 @@
-export { Encoder } from "./lib/Encoder.ts";
+export { encode, Encoder } from "./lib/Encoder.ts";
 export { decode, Decoder } from "./lib/Decoder.ts";
 export * from "./lib/Constants.ts";
