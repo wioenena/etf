95c7e6ed8993527fe2e787ffe58214079dc06e13
wioenena-q <wioenena@gmail.com>
2023-03-07 03:29:05 +0300

    test: atom encoding tested

--- deno/tests/Encoder.test.ts
+++ deno/tests/Encoder.test.ts
@@ -1,4 +1,4 @@
-import { decode, encode } from "../mod.ts";
+import { Atom, decode, encode } from "../mod.ts";
 import * as Constants from "../lib/Constants.ts";
 import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 
@@ -7,5 +7,14 @@ Deno.test("Encoder", async (t) => {
     const encoded = encode(null);
     assertEquals(encoded[1], Constants.ATOM_EXT);
     assertEquals(decode(encoded), null);
+  });
+
+  await t.step("encode Atom", () => {
+    const encodedWithUTF8 = encode(new Atom("my_atom", true));
+    const encodedWithoutUTF8 = encode(new Atom("my_atom"));
+    assertEquals(encodedWithUTF8[1], Constants.ATOM_UTF8_EXT);
+    assertEquals(encodedWithoutUTF8[1], Constants.ATOM_EXT);
+    assertEquals(decode(encodedWithUTF8), "my_atom");
+    assertEquals(decode(encodedWithoutUTF8), "my_atom");
   });
 });
