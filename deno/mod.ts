6477985495c5611785993038b9cf359e29805b5b
wioenena-q <wioenena@gmail.com>
2023-03-07 03:18:49 +0300

    feat: exported atom

--- deno/mod.ts
+++ deno/mod.ts
@@ -1,3 +1,4 @@
 export { encode, Encoder } from "./lib/Encoder.ts";
 export { decode, Decoder } from "./lib/Decoder.ts";
+export { Atom } from "./lib/Structs/Atom.ts";
 export * from "./lib/Constants.ts";
