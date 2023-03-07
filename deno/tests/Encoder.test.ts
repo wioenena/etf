adffac0845bf83ff086fe99a05e01ffefac05895
wioenena-q <wioenena@gmail.com>
2023-03-07 03:27:48 +0300

    test: null encoding tested

--- deno/tests/Encoder.test.ts
+++ deno/tests/Encoder.test.ts
@@ -1,1 +1,11 @@
-Deno.test("Encoder", () => {});
+import { decode, encode } from "../mod.ts";
+import * as Constants from "../lib/Constants.ts";
+import { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
+
+Deno.test("Encoder", async (t) => {
+  await t.step("encode null", () => {
+    const encoded = encode(null);
+    assertEquals(encoded[1], Constants.ATOM_EXT);
+    assertEquals(decode(encoded), null);
+  });
+});
